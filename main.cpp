6fd41e3a2ad37b3150df2291324e8a9b9b6c99b4
d0m G <dginhac@u-bourgogne.fr>
2023-10-20 11:12:10 +0200

    ajout class Person avec prenom, nom et genre

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
 #include "date.h"
+#include "person.h"
 
 int main() {
   Date date(10, 14);
   std::cout << "month: " << date.month() << std::endl;
   std::cout << "day: " << date.day() << std::endl;
+
+  Person person("John", "Doe", 1);
+  std::cout << "Full name: " << person.getFullName() << std::endl;
+  
   return 0;
 }
